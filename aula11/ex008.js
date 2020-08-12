50e646731499b874c8aadf87ff4c77d610c56c66
VandairCorreiaCabral <riadnav@gmail.com>
2020-08-12 11:16:27 -0300

    limpando para fazer bransch
    apageui as pastas das aulas para fazer branch de cada alula

--- aula11/ex008.js
+++
@@ -1,8 +0,0 @@
-var vel = 60.5
-console.log(`voce esta na velocidade ${vel}`)
-if (vel >=80) {
-    console.log(`voce esta acima da veloxlidade`)
-    
-}else{
-    console.log(`voce esta abaixo da velocidade`)
-}
--- aula11/ex009.js
+++
@@ -1,8 +0,0 @@
-var pais = 'EUA'
-console.log(`vevendo em ${pais}`)
-if (pais == 'Brasil') {
-    console.log('voce e brasileiro')
-        
-}else{
-    console.log('voce e estrangeiro')
-}
