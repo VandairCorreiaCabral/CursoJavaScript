8b45cb19b3e6644abdd8dbbc7f10d52ce37d7758
VandairCorreiaCabral <riadnav@gmail.com>
2020-08-12 15:21:12 -0300

    add aula01
    criei e finalizei a aula 11

---
+++ aula11/ex008.js
@@ -0,0 +1,8 @@
+var vel = 60.5
+console.log(`voce esta na velocidade ${vel}`)
+if (vel >=80) {
+    console.log(`voce esta acima da veloxlidade`)
+    
+}else{
+    console.log(`voce esta abaixo da velocidade`)
+}
---
+++ aula11/ex009.js
@@ -0,0 +1,8 @@
+var pais = 'EUA'
+console.log(`vevendo em ${pais}`)
+if (pais == 'Brasil') {
+    console.log('voce e brasileiro')
+        
+}else{
+    console.log('voce e estrangeiro')
+}
